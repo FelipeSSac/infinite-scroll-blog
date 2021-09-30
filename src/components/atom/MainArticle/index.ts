ab0bbeabb36e5ca02fe505dbf075f906028cc211
Felipe Souza <82123850+FelipeSSac@users.noreply.github.com>
2021-09-29 21:46:19 -0300

    Revert "Feature/infinite scroll page"

--- src/components/atom/MainArticle/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MainArticle } from './MainArticle';
--- src/helpers/ensures/ensureSummary.ts
+++
@@ -1,9 +0,0 @@
-export default function ensureSummary(value: string) {
-  return value
-    .replace('<p>', '')
-    .split('</p>')[0]
-    .split('.')
-    .slice(0, 2)
-    .join('.')
-    .concat('.');
-}
